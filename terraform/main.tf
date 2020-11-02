3867d3f37bdd8bcd8d23bd31541764ec98fd5c5e
unknown <angelotebh@hotmail.com>
2020-11-02 17:59:46 +0000

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,14 @@
 
 provider "google" {
   project = var.project_id
-  # credentials = file("c:/dev/cloud_buildmoviestelegrambot-2a119e82adee.json")
 }
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
 }
 
-resource "google_cloud_run_service" "my-service" {
-  name     = "my-service"
+resource "google_cloud_run_service" "movies-telegram-bot" {
+  name     = "movies-telegram-bot"
   location = "us-central1"
 
   template {
@@ -34,12 +33,12 @@ resource "google_cloud_run_service" "my-service" {
 }
 
 resource "google_cloud_run_service_iam_member" "allUsers" {
-  service  = google_cloud_run_service.my-service.name
-  location = google_cloud_run_service.my-service.location
+  service  = google_cloud_run_service.movies-telegram-bot.name
+  location = google_cloud_run_service.movies-telegram-bot.location
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 output "url" {
-  value = google_cloud_run_service.my-service.status[0].url
+  value = google_cloud_run_service.movies-telegram-bot.status[0].url
 }
